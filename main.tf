956ada9a9a55878be2ac99d899028eef8c323ff6
Tomochika Kato <139940984+katofw42@users.noreply.github.com>
2025-03-20 14:31:59 +0000

    use pms

--- main.tf
+++ main.tf
@@ -1,11 +1,15 @@
 # test
 
 provider "aws" {
-  region = "us-west-2" # Updated region
+  region = "us-west-2"
+}
+
+data "aws_ssm_parameter" "amazon_linux_2023_ami" {
+  name = "/aws/service/ami-amazon-linux-latest/al2023-ami-kernel-default-x86_64"
 }
 
 resource "aws_instance" "example" {
-  ami           = "ami-0c55b159cbfafe1f0" # Updated AMI ID
+  ami           = data.aws_ssm_parameter.amazon_linux_2023_ami.value
   instance_type = "t3.micro"
 
   tags = {
