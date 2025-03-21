04e6435d09d623284fd38eb3d5dddfa6a346dd8c
Tomochika Kato <139940984+katofw42@users.noreply.github.com>
2025-03-21 05:57:30 +0000

    instance

--- main.tf
+++ main.tf
@@ -1,3 +1,16 @@
 provider "aws" {
   region = "us-west-2"
 }
+
+data "aws_ssm_parameter" "amazon_linux_2023_ami" {
+  name = "/aws/service/ami-amazon-linux-latest/al2023-ami-kernel-default-x86_64"
+}
+
+resource "aws_instance" "amazon_linux_2023" {
+  ami           = data.aws_ssm_parameter.amazon_linux_2023_ami.value
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "AmazonLinux2023Instance"
+  }
+}
